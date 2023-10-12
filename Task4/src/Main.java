7051e207800e6236ae2843f91e64a4574060b2ee
123456 <napnest@gmail.com>
2023-10-12 14:32:44 +0300

    All tasks complete

--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo Numbers
+        String telephoneNumber = new Scanner(System.in).nextLine();
+        telephoneNumber = telephoneNumber.replaceAll("[\\D\\s\\+\\-()]", "").trim();
+        telephoneNumber = telephoneNumber.replaceAll("^8", "7");
+        boolean flag = true;
+        if (telephoneNumber.length() == 10) {
+            telephoneNumber = "7" + telephoneNumber;
+        }
+        if ((telephoneNumber.length() == 11) &&
+                !((telephoneNumber.startsWith("7") || (telephoneNumber.startsWith("8"))))) {
+            System.out.println("Phone number is invalid");
+            flag=false;
+        }
+        if (telephoneNumber.length() != 11) {
+            System.out.println("Phone number is ivalid");
+            flag=false;
+        }
+        if(flag) {
+            telephoneNumber=telephoneNumber.replaceAll("(^\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})"
+            ,"+$1 ($2) $3 $4-$5");
+            System.out.print("Your phone number is: "+telephoneNumber);
+        }
     }
 }

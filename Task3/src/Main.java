13aeb02b509cf1d0a601d999dc46d905c640dd6a
123456 <napnest@gmail.com>
2023-10-12 16:43:53 +0300

    All tasks complete

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,12 +1,16 @@
+//Ввести ФИО и вывести на экран, используя регулярные выражения
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //вводим ФИО
         String FIO = sc.nextLine().trim();
+        //соответствие ФИО регулярному выражению (только кириллица, 3 слова)
         if (!FIO.matches("([А-яЁё]+\\s){2}([А-яЁё]+)")){
             System.out.println("Введенная строка не является ФИО");
         }
+        //записываем ФИО отдельно в переменные и выводим на экран
         else {
             String familia = FIO.replaceAll("\\s[А-яЁё]+"," ").trim();
             String name = FIO.replaceAll("(^[А-яЁё]+\\s)|(\\s[А-яЁё]+$)"," ").trim();
--- Task4/src/Main.java
+++ Task4/src/Main.java
@@ -1,23 +1,30 @@
+//телефонный номер через регулярные выражения (11 цифр)
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //ввод телефонного номера
         String telephoneNumber = new Scanner(System.in).nextLine();
+        //убираем лишние символы при вводе, если номер начинается с 7 заменяем на 8
         telephoneNumber = telephoneNumber.replaceAll("[\\D\\s\\+\\-()]", "").trim();
         telephoneNumber = telephoneNumber.replaceAll("^8", "7");
         boolean flag = true;
+        //если цифр 10 добавляем 7 в начале строки
         if (telephoneNumber.length() == 10) {
             telephoneNumber = "7" + telephoneNumber;
         }
+        //если длина 11, но начинается не с 8 или 7 выводим на экран сообщение
         if ((telephoneNumber.length() == 11) &&
                 !((telephoneNumber.startsWith("7") || (telephoneNumber.startsWith("8"))))) {
             System.out.println("Phone number is invalid");
             flag=false;
         }
+        //если длина не соответствует 11 выводим сообщение
         if (telephoneNumber.length() != 11) {
             System.out.println("Phone number is ivalid");
             flag=false;
         }
+        //если условия верны выводим номер по заданному шаблону
         if(flag) {
             telephoneNumber=telephoneNumber.replaceAll("(^\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})"
             ,"+$1 ($2) $3 $4-$5");

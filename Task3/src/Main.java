6c8c1ef8ab61738f64a7b7f3e69232fee90babda
123456 <napnest@gmail.com>
2023-10-12 12:49:08 +0300

    Task 2 complete

--- Task3/src/Main.java
+++ Task3/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo ФИО
+        Scanner sc = new Scanner(System.in);
+        String FIO = sc.nextLine().trim();
+        if (!FIO.matches("([А-яЁё]+\\s){2}([А-яЁё]+)")){
+            System.out.println("Введенная строка не является ФИО");
+        }
+        else {
+            String familia = FIO.replaceAll("\\s[А-яЁё]+"," ").trim();
+            String name = FIO.replaceAll("(^[А-яЁё]+\\s)|(\\s[А-яЁё]+$)"," ").trim();
+            String otchestvo = FIO.replaceAll("(^[А-я]+)(\\s[А-я]+\\s)"," ").trim();
+            System.out.println("Фамилия: "+ familia);
+            System.out.println("Имя: "+name);
+            System.out.println("Отчество: "+otchestvo);
+        }
+
     }
 }
